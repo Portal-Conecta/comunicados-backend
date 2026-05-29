1e6fc3cb3b4f636ff33100ae7df8013eeba18baa
jonathan_uber <jonathanuber121207@gmail.com>
2026-05-28 21:05:41 -0300

    #20 feat: cria entidade AnnouncementFile e implementa seus respectivos atributos

---
+++ src/main/java/com/portal/conecta/comunicados/module/comunicado/domain/model/AnnouncementFile.java
@@ -0,0 +1,65 @@
+package com.portal.conecta.comunicados.module.comunicado.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+
+import com.portal.conecta.comunicados.module.comunicado.domain.enums.AnnouncementFileType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "announcement_file")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AnnouncementFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "announcement_id", nullable = false)
+    private Announcement announcement;
+
+    @Column(name = "original_name", nullable = false)
+    private String originalName;
+
+    @Column(name = "s3_key", nullable = false)
+    private String s3Key;
+
+    @Column(name = "s3_bucket", nullable = false)
+    private String s3Bucket;
+
+    @Column(name = "content_type", nullable = false)
+    private String contentType;
+
+    @Column(name = "type", nullable = false)
+    private AnnouncementFileType type;
+
+    @Column(name = "size_bytes", nullable = false)
+    private Long sizeBytes;
+
+    @Column(name = "is_thumbnail", nullable = false)
+    private boolean isThumbnail;
+
+    @Column(name = "uploaded_by_user_id", nullable = false)
+    private UUID uploadedByUserId;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+    
+}
