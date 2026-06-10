a2b49e1b7919d404da3fff1da686867e3542a2f3
bruno_l_medeiros <brunoluismedeiros.08@gmail.com>
2026-06-10 16:04:31 -0300

    feat: adiciona configuração OpenAPI e guia de documentação Swagger

---
+++ src/main/java/com/portal/conecta/comunicados/shared/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.portal.conecta.comunicados.shared.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    public static final String BEARER_AUTH_SCHEME = "bearerAuth";
+
+    @Bean
+    public OpenAPI comunicadosOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Comunicados API")
+                        .description("Documentação navegável dos contratos HTTP do módulo de Comunicados.")
+                        .version("0.0.1-SNAPSHOT"))
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_AUTH_SCHEME, new SecurityScheme()
+                                .name(BEARER_AUTH_SCHEME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
