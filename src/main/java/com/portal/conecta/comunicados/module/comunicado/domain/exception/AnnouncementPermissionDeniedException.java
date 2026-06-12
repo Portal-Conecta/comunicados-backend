0b260b5479e41b2c9dd804c667282b2ac890c32c
jonathan_uber <jonathanuber121207@gmail.com>
2026-06-12 14:17:43 -0300

    #103 feat: padroniza erros de inglês para português para seguir o padrão do projeto

--- src/main/java/com/portal/conecta/comunicados/module/comunicado/domain/exception/AnnouncementPermissionDeniedException.java
+++ src/main/java/com/portal/conecta/comunicados/module/comunicado/domain/exception/AnnouncementPermissionDeniedException.java
@@ -2,6 +2,6 @@ package com.portal.conecta.comunicados.module.comunicado.domain.exception;
 
 public class AnnouncementPermissionDeniedException extends RuntimeException {
     public AnnouncementPermissionDeniedException() {
-        super("User does not have permission to create an announcement.");
+        super("Usuário não tem permissão para criar um comunicado.");
     }
 }
--- src/main/java/com/portal/conecta/comunicados/module/tag/domain/exception/TagPermissionDeniedException.java
+++ src/main/java/com/portal/conecta/comunicados/module/tag/domain/exception/TagPermissionDeniedException.java
@@ -2,6 +2,6 @@ package com.portal.conecta.comunicados.module.tag.domain.exception;
 
 public class TagPermissionDeniedException extends RuntimeException {
     public TagPermissionDeniedException() {
-        super("Usuário não tem permissão para associar / desassociar tags neste comunicado.");
+        super("Usuário não tem permissão para associar/desassociar tags neste comunicado.");
     }
 }
